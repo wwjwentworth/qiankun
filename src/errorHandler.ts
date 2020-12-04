df2d547a2bee404eab9cdb9d01fb25e500b4a130
wuwenjie <wuwenjie@xiaomai5.com>
2020-12-04 16:02:27 +0800

    feat: 全局未捕获异常处理

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -5,11 +5,13 @@
 
 export { addErrorHandler, removeErrorHandler } from 'single-spa';
 
+// 监听了error和unhandledrejection事件
 export function addGlobalUncaughtErrorHandler(errorHandler: OnErrorEventHandlerNonNull): void {
   window.addEventListener('error', errorHandler);
   window.addEventListener('unhandledrejection', errorHandler);
 }
 
+// 移除了error和unhandledrejection事件
 export function removeGlobalUncaughtErrorHandler(errorHandler: (...args: any[]) => any) {
   window.removeEventListener('error', errorHandler);
   window.removeEventListener('unhandledrejection', errorHandler);
